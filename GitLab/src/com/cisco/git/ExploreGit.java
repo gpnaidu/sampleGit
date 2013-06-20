96e046a8afd71d35ff4ee26d175c07197c97b1ab
pganne <pganne@PGANNE-WS01.partnet.cisco.com>
2013-06-21 02:20:36 +0530

    Committed new Project.

---
+++ GitLab/src/com/cisco/git/ExploreGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.cisco.git;
+
+/**
+ * @author pganne
+ *
+ */
+public class ExploreGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is Easy");
+	}
+
+}
